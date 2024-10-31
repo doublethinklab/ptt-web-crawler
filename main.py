7c0fe7abada7ac1a438e122948ebeadd7765868e
andyfcx <dreamcastle.andy@gmail.com>
2024-10-31 17:54:35 +0800

    handle exception

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ client = pymongo.MongoClient(os.getenv('MONGODB_URI'))
 dtl_data = client['dtl_data']
 ptt_data = dtl_data['ptt_data']
 def to_mongo(data):
-    bulk_operations = []
-    if data:
-        for article in data:
-            bulk_operations.append(UpdateOne(
-                {'article_id': article.get('article_id')},
-                {'$set': article},
-                upsert=True
-            ))
-        ptt_data.bulk_write(bulk_operations)
+    try:
+        bulk_operations = []
+        if data:
+            for article in data:
+                bulk_operations.append(UpdateOne(
+                    {'article_id': article.get('article_id')},
+                    {'$set': article},
+                    upsert=True
+                ))
+            ptt_data.bulk_write(bulk_operations)
+    except Exception as e:
+        print(e)
 
 c = PttWebCrawler(as_lib=True)
 to_mongo(c.parse_articles(0, 10, 'joke', save_locally=True))
